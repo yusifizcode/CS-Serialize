beac0ebad078afc8b1785574bd47fec87c80952c
yusifizcode <5ba0gh2@code.edu.az>
2022-04-14 14:00:31 +0400

    serialize

---
+++ CSharp-Serialize/Program.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace CSharp_Serialize
+{
+    class Program
+    {
+        static string _path = @"C:\Users\Yusifiz\Desktop\files";
+
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                User user1 = new User
+                {
+                    Name = "Ibrahim",
+                    Surname = "Abbasov",
+                    Age = 40
+                };
+            }
+            static void SerializeDat(User user)
+            {
+                using (FileStream fileStream = new FileStream(_path + "user1.dat", FileMode.Create))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    binaryFormatter.Serialize(fileStream, user);
+                }
+            }
+
+            static User DeserializeDat()
+            {
+                object data;
+                using (FileStream fileStream = new FileStream(_path + "user1.dat", FileMode.Open))
+                {
+                    BinaryFormatter binaryFormatter = new BinaryFormatter();
+                    data = binaryFormatter.Deserialize(fileStream);
+                }
+                return (User)data;
+            }
+
+            static void SerializeJson(User user)
+            {
+                var objectStr = JsonConvert.SerializeObject(user);
+                using (FileStream fileStream = new FileStream(_path + "user2.json", FileMode.Create))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(fileStream))
+                    {
+                        streamWriter.Write(objectStr);
+                    }
+                }
+            }
+
+            static User DeserializeJson()
+            {
+                string userObjStr;
+                using (FileStream fileStream = new FileStream(_path + "user2.json", FileMode.Open))
+                {
+                    StreamReader streamReader = new StreamReader(fileStream);
+                    userObjStr = streamReader.ReadToEnd();
+                }
+
+                var user = JsonConvert.DeserializeObject<User>(userObjStr);
+                return user;
+            }
+
+            static void SerializeXml(User user)
+            {
+                using (FileStream stream = new FileStream(_path + "user3.xml", FileMode.Create))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+                    xmlSerializer.Serialize(stream, user);
+                }
+            }
+
+            static User DeserializeXml()
+            {
+                object userObj;
+                using (FileStream stream = new FileStream(_path + "user3.xml", FileMode.Open))
+                {
+                    XmlSerializer xmlSerializer = new XmlSerializer(typeof(User));
+                    userObj = xmlSerializer.Deserialize(stream);
+                }
+
+                return (User)userObj;
+            }
+        }
+
+    }
+}
---
+++ CSharp-Serialize/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharp_Serialize
+{
+    class User
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Age { get; set; }
+    }
+}
